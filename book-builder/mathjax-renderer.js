3e212985a86780e17a2b8ab6535a49de625d211a
Marcin Serwin <toxyxer@gmail.com>
2020-08-18 17:23:41 +0200

    Ouput latex into png instead of svg

--- book-builder/mathjax-renderer.js
+++ book-builder/mathjax-renderer.js
@@ -1,20 +1,21 @@
 'use strict';
 
+const MathJax = require("mathjax-node-svg2png")
 const mjAPI = require("mathjax-node-page");
 const jsdom = require("jsdom").JSDOM;
 
-function tryStartApi() {
-    if (!tryStartApi.apiStarted) {
-        mjAPI.start();
-        tryStartApi.apiStarted = true;
-    }
-}
-tryStartApi.apiStarted = false;
+let _initialized = false;
 
 exports.render = function (html, callback) {
     const document = (new jsdom(html, { features: { FetchExternalResources: false } })).window.document;
+    if (!_initialized) {
+        mjAPI.init(MathJax);
+        _initialized = true;
+    }
+    mjAPI.addOutput("png", (wrapper, png) => { wrapper.innerHTML = `<img src="${png}">` })
     mjAPI.mjpage(document.body.innerHTML, { MathJax: { SVG: { font: 'TeX' } }, extensions: 'TeX/noUndefined' }, {
-        svg: true,
+        png: true,
+        scale: 1.7,
         ex: 6.1, width: 200,
         linebreaks: true,
         singleDollars: false
